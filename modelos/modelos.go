b2b4d381d2c279a4a7d5b4fa727924f0f6b7894a
Ítalo Lemos Gasparo <71532764+ItaloLemosGasparo@users.noreply.github.com>
2023-03-24 20:16:00 -0300

    Update modelos.go

--- modelos/modelos.go
+++ modelos/modelos.go
@@ -11,3 +11,56 @@ type Usuario struct {
 	CPF        string
 	Privilegio string
 }
+
+type Endereco struct {
+	Id_Usuario int
+	Logradouro string
+	Numero     int
+	Bairro     string
+	Cidade     string
+	Uf         string
+	Cep        string
+}
+
+type Senhas struct {
+	Id_Usuario int
+	SenhaA     string
+	SenhaB     string
+}
+
+type Fornecedor struct {
+	gorm.Model
+	Nome      string
+	Email     string
+	Telefone  string
+	TelefoneB string
+	Cpf       string
+	Cnpj      string
+}
+
+type Produto struct {
+	gorm.Model
+	Id_Fornecedor int
+	Nome          string
+	Descricao     string
+	Preco         float32
+}
+
+type Carrinho struct {
+	Id         int
+	Id_Usuario int
+}
+
+type Items_Carrinho struct {
+	Id          int
+	Id_Carrinho int
+	ID_Produto  int
+	Quantidade  float32
+}
+
+type Pedido struct {
+	Id          int
+	Id_Carrinho int
+	Id_Usuario  int
+	Situacao    string
+}
