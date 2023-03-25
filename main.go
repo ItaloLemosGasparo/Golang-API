bd4715fcc0e418b0ef2103557765b23fc9ae995a
ItaloLemosGasparo <italolemos4@gmail.com>
2023-03-25 08:39:41 -0300

    editado o arquivo main.go

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	fmt.Print("Funcionou meu chapa")
+}
+
+func main() {
+	r := gin.Default()
+
+	//r.POST("/posts", controladores.CriarPost)
+	//r.GET("/posts", controladores.BuscarPosts)
+	//r.GET("/posts/:id", controladores.BuscarPost)
+	//r.PUT("/posts/:id", controladores.AtualizarPost)
+	//r.DELETE("/posts/:id", controladores.DeletarPost)
+
+	r.Run()
+}
