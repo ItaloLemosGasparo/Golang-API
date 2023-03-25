4244155178b638d0921e7950a13ef5d310eec7a3
ItaloLemosGasparo <italolemos4@gmail.com>
2023-03-25 09:15:10 -0300

    Inicializadores

--- controladores/controladoresUsuario.go
+++ controladores/controladoresUsuario.go
@@ -1,1 +1,16 @@
 package controladores
+
+import (
+	"projeto_def/inicializadores"
+	"projeto_def/modelos"
+
+	"github.com/gin-gonic/gin"
+)
+
+func deletarUsuario(c *gin.Context) {
+	id := c.Param("id")
+
+	inicializadores.BD.Delete(&modelos.Usuario{}, id)
+
+	c.Status(200)
+}
